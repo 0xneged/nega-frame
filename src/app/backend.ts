e80a49aeb290bd3398fa0d35d0e08e3c69de4486
T_DaMER <dpancov64@gmail.com>
2024-07-04 23:35:06 +0300

    use &embeds[]

--- src/app/backend.ts
+++ src/app/backend.ts
@@ -12,5 +12,7 @@ export async function addUser(address: string) {
 }
 
 export function shareOnWarpCast() {
-  return `https://warpcast.com/~/compose?text=I just claimed 1000 HATs from @neged! ${env.SITE_URL}`
+  return encodeURIComponent(
+    `https://warpcast.com/~/compose?text=I just claimed 1000 HATs from @neged!&embeds[]=${env.SITE_URL}`
+  )
 }
